5fd1b4765500b95b0e197a7c8e287dec89ddee15
elisa lee <elisa.my.lee@gmail.com>
2016-10-21 11:33:35 -0500

    initial files & server set up

---
+++ public/scripts/app.config.js
@@ -0,0 +1,14 @@
+angular.module('characterApp')
+       .config(function ($routeProvider, $locationProvider) {
+        $routeProvider.when('/home', {
+          templateUrl: 'views/home.html',
+        }).when('/gabumon', {
+          templateUrl: 'views/gabumon.html',
+        }).when('/gatomon', {
+          templateUrl: 'views/gatomon.html',
+        }).when('/leomon', {
+          templateUrl: 'views/leomon.html',
+        });
+
+        $locationProvider.html5Mode(true);
+      });
